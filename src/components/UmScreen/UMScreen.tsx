ea2bdfa5e7be4a63090d8cb1af2a4c41482ca81e
Kymarndo <49214298+Kymarndo@users.noreply.github.com>
2022-11-08 22:23:34 +1300

    User management form created

--- src/components/UmScreen/UMScreen.tsx
+++ src/components/UmScreen/UMScreen.tsx
@@ -1,4 +1,4 @@
-
+import styles from './UMScreen.module.css'
 import React, { useState } from 'react'
 import CreatableSelect from 'react-select/creatable';
 
@@ -26,13 +26,26 @@ interface Option {
     createOption('Salesforce Developer'),
   ];
 
+  const roleOptions = [
+    createOption('Candidate'),
+    createOption('Trainer'),
+    createOption('Admin'),
+  ];
+
 
 export default function UMScreen() {
 
         const [isLoading, setIsLoading] = useState(false);
         const [inOptions, setInOptions] = useState(intakeOptions);
         const [paOptions, setPaOptions] = useState(pathOptions);
+        const [roOptions1, setRoOptions1] = useState(roleOptions);
+        const [roOptions2, setRoOptions2] = useState(roleOptions);
+        const [roOptions3, setRoOptions3] = useState(roleOptions);
         const [value, setValue] = useState<Option | null>();
+        const [value2, setValue2] = useState<Option | null>();
+        const [roleValue1, setRoleValue1] = useState<Option | null>();
+        const [roleValue2, setRoleValue2] = useState<Option | null>();
+        const [roleValue3, setRoleValue3] = useState<Option | null>();
 
         const handleCreate = (inputValue: string) => {
             setIsLoading(true);
@@ -50,12 +63,46 @@ export default function UMScreen() {
               const newOption = createOption(inputValue);
               setIsLoading(false);
               setPaOptions((prev) => [...prev, newOption]);
+              setValue2(newOption);
+            }, 1000);
+          };
+
+          const handleRoleCreate1 = (inputValue: string) => {
+            setIsLoading(true);
+            setTimeout(() => {
+              const newOption = createOption(inputValue);
+              setIsLoading(false);
+              setRoOptions1((prev) => [...prev, newOption]);
+              setValue(newOption);
+            }, 1000);
+          };
+
+          const handleRoleCreate2 = (inputValue: string) => {
+            setIsLoading(true);
+            setTimeout(() => {
+              const newOption = createOption(inputValue);
+              setIsLoading(false);
+              setRoOptions2((prev) => [...prev, newOption]);
+              setValue(newOption);
+            }, 1000);
+          };
+
+          const handleRoleCreate3 = (inputValue: string) => {
+            setIsLoading(true);
+            setTimeout(() => {
+              const newOption = createOption(inputValue);
+              setIsLoading(false);
+              setRoOptions3((prev) => [...prev, newOption]);
               setValue(newOption);
             }, 1000);
           };
     return ( 
       <>
+      <div className={styles.upperSection}>
+        <div className={styles.selectionSection}>
+      <h3 className={styles.titles}>Select Intake</h3>
       <CreatableSelect
+      className={styles.selector}
       isClearable
       isDisabled={isLoading}
       isLoading={isLoading}
@@ -64,16 +111,78 @@ export default function UMScreen() {
       options={inOptions}
       value={value}
     />
-
+    </div>
+    <div className={styles.selectionSection}>
+    <h3 className={styles.titles}>Select Pathway</h3>
     <CreatableSelect
+          className={styles.selector}
     isClearable
     isDisabled={isLoading}
     isLoading={isLoading}
-    onChange={(newValue) => setValue(newValue)}
+    onChange={(newValue) => setValue2(newValue)}
     onCreateOption={handleCreate2}
     options={paOptions}
-    value={value}
+    value={value2}
   />
+  </div>
+  </div>
+  <div className={styles.lowerSection}>
+     <div>First Name: <input placeholder="First Name" className={styles.formInputs}/></div> 
+     <div> Last Name: <input placeholder="Last Name" className={styles.formInputs}/></div> 
+     <div> Email:<input placeholder="Email" className={styles.formInputs}/></div> 
+     <div>
+      <div>Role:
+     <CreatableSelect
+          className={styles.formSelector}
+    isClearable
+    isDisabled={isLoading}
+    isLoading={isLoading}
+    onChange={(newValue) => setRoleValue1(newValue)}
+    onCreateOption={handleRoleCreate1}
+    options={roOptions1}
+    value={roleValue1}
+  />
+  </div>
+  </div>
+  </div>
+  <div className={styles.lowerSection}>
+     <div>First Name: <input placeholder="First Name" className={styles.formInputs}/></div> 
+     <div> Last Name: <input placeholder="Last Name" className={styles.formInputs}/></div> 
+     <div> Email:<input placeholder="Email" className={styles.formInputs}/></div> 
+     <div>
+      <div>Role:
+     <CreatableSelect
+          className={styles.formSelector}
+    isClearable
+    isDisabled={isLoading}
+    isLoading={isLoading}
+    onChange={(newValue) => setRoleValue2(newValue)}
+    onCreateOption={handleRoleCreate2}
+    options={roOptions2}
+    value={roleValue2}
+  />
+  </div>
+  </div>
+  </div>
+  <div className={styles.lowerSection}>
+     <div>First Name: <input placeholder="First Name" className={styles.formInputs}/></div> 
+     <div> Last Name: <input placeholder="Last Name" className={styles.formInputs}/></div> 
+     <div> Email:<input placeholder="Email" className={styles.formInputs}/></div> 
+     <div>
+      <div>Role:
+     <CreatableSelect
+          className={styles.formSelector}
+    isClearable
+    isDisabled={isLoading}
+    isLoading={isLoading}
+    onChange={(newValue) => setRoleValue3(newValue)}
+    onCreateOption={handleRoleCreate3}
+    options={roOptions3}
+    value={roleValue3}
+  />
+  </div>
+  </div>
+  </div>
   </>
     )
 }
