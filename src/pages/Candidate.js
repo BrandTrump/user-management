9a7ccfc9fb0a3de5c0e15c66bd8cf598f04a547c
Brandon Trump <brandontrump101@gmail.com>
2022-12-06 10:55:45 +1300

    added a link going straight to candidate submissions

--- src/pages/Candidate.js
+++ src/pages/Candidate.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import MicrosoftSignOut from "../components/auth/MicrosoftSignOut";
 
 const Candidate = () => {
   return (
     <>
       <h1>Candidate Page</h1>
+      <Link to="/candidate/submission">Submit Work</Link>
       <MicrosoftSignOut />
     </>
   );
