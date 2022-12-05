7fc72cf0391106aeb389ec1e2dc6e7fe8207e62d
Brandon Trump <brandontrump101@gmail.com>
2022-12-06 10:57:24 +1300

    created a page to view submissions based on candidate names and mission number

---
+++ src/pages/MarkingPage.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+import MicrosoftSignOut from "../components/auth/MicrosoftSignOut";
+import ViewSubmission from "../components/FileUpload/ViewSubmission";
+
+const MarkingPage = () => {
+  const { candidate, id } = useParams();
+  return (
+    <div>
+      <ViewSubmission candidateName={candidate} missionNum={id} />
+      <MicrosoftSignOut />
+    </div>
+  );
+};
+
+export default MarkingPage;
