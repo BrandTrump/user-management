033732454e2706265a180f31587b18baaa2138b9
Brandon Trump <brandontrump101@gmail.com>
2022-12-11 21:24:51 +1300

    created new page for selecting a mission to mark

---
+++ src/pages/SelectMissionForMarking.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MissionSelect from "../components/ViewSubmissions/MissionSelect";
+
+const SelectMissionForMarking = () => {
+  return (
+    <div>
+      <MissionSelect />
+    </div>
+  );
+};
+
+export default SelectMissionForMarking;
