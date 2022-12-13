bbc09ffd2aa251db03f36fb055569a69054ace2d
Brandon Trump <brandontrump101@gmail.com>
2022-12-14 10:04:06 +1300

    removed warnings

--- src/pages/PublishRubric.js
+++ src/pages/PublishRubric.js
@@ -1,15 +1,13 @@
 import MicrosoftSignOut from "../components/auth/MicrosoftSignOut";
-import { useState } from "react";
+
 import PublishRubricComponent from "../components/rubric-template/PublishRubric.tsx";
 
-export default function LearningObjectives () {
-    const [isAuth, setIsAuth] = useState(false);
-    
-    return (
-        <div>
-            <PublishRubricComponent />
-        Lol2
-        <MicrosoftSignOut setIsAuth={setIsAuth} />
-        </div>
-        );
+export default function LearningObjectives() {
+  return (
+    <div>
+      <PublishRubricComponent />
+      Lol2
+      <MicrosoftSignOut />
+    </div>
+  );
 }
